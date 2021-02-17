6142e303f0e59dd734790f7fbb3ba868c28a505f
Chris Russell <chris.john.russell@gmail.com>
2021-02-17 12:12:01 +0000

    Adds case queries

--- case.sql
+++ case.sql
@@ -1,1 +1,17 @@
--- CASE WHEN, works like if/else
+-- CASE WHEN, works like if/else for each row
+-- Use before the FROM statement
+-- Is therefore a filter on column selection prior to WHERE
+
+SELECT player_name,
+       year,
+       CASE WHEN year = 'SR' THEN 'yes'
+       ELSE 'Not a senior' END
+       AS is_a_senior
+  FROM benn.college_football_players
+
+  SELECT player_name, state,
+  CASE WHEN state = 'CA' THEN 'yes'
+  ELSE 'no' END
+  AS from_CA
+  FROM benn.college_football_players
+  LIMIT 300
